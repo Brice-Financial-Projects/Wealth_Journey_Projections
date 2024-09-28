f9134312fcace2968416b8fbd9070c33ae241e05
bnelson <bnelsonemail@icloud.com>
2024-09-28 01:59:40 -0400

    reorganized the text files, added functions to main.py for opening and reading data files, allowing use of default values

--- main.py
+++ main.py
@@ -11,4 +11,44 @@ returns, volatility, and time horizons on retirement savings outcomes.
 Developed in the VS Code editor.
 """
 
+# import sys, random
+# import matplotlib.pyplot as plt
+
+
+def read_to_list(file_name):
+    """Open a file of data in percent, convert to decimal & return a list.
+
+    Args:
+        file_name (str): The name of the file containing percentage values.
+    
+    Returns:
+        list: A list of decimal values converted from percentages.
+    """
+    with open (file_name) as in_file:
+        lines = [float(line.strip()) for line in in_file]
+        decimal = [round(line / 100, 5) for line in lines]
+        return decimal
+    
+def default_input(prompt, default=None):
+    """Prompt the user for input, allowing the use of a default value.
+
+    Args:
+        prompt (str): The message to display to the user when asking for
+            input.
+        default (str, optional): The default value to use if no input is
+            provided. Defaults to None.
+
+    Returns:
+        str: The user's input, or the default value if no input is provided.
+    """
+
+    prompt = '{} [{}]: '.format(prompt, default)
+    response = input(prompt)
+    if not response and default:
+        return default
+    else:
+        return response
+
+
+    
 
