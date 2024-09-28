b2e7a1644acddddb5791c7553ba9d7dc243aa9de
bnelson <bnelsonemail@icloud.com>
2024-09-28 01:31:59 -0400

    addition of text files that contains historical data, addition of psuedocode.txt

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-"""_summary.
-    
-VS Coded Editor
-
-This is a python Monte Carlo Simulation of Retirement Savings Simulation
-script file.
 """
+Monte Carlo Simulation for Retirement Savings
+
+This Python script simulates the growth of retirement savings using a
+Monte Carlo method. It estimates potential outcomes of different investment
+scenarios by running numerous simulations, each with varying market
+conditions. The script helps model the impact of factors such as annual
+returns, volatility, and time horizons on retirement savings outcomes.
+
+Developed in the VS Code editor.
+"""
+
 
